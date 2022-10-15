72ec8f9c98159019efcc655b77f21e8fe2538eb9
Todd <e-royal@163.com>
2022-10-15 22:54:34 +0800

    对bootsect.s，head.s进行了修改。

--- code/boot/bootsect.s
+++ code/boot/bootsect.s
@@ -7,7 +7,7 @@
 #
 #include <linux/config.h>
 
-	.equ SYSSIZE, DEF_SYSSIZE
+.equ SYSSIZE, DEF_SYSSIZE
 #
 #	bootsect.s		(C) 1991 Linus Torvalds
 #
@@ -26,47 +26,48 @@
 # read errors will result in a unbreakable loop. Reboot by hand. It
 # loads pretty fast by getting whole sectors at a time whenever possible.
 
-	.global _start, begtext, begdata, begbss, endtext, enddata, endbss
-	.text
-	begtext:
-	.data
-	begdata:
-	.bss
-	begbss:
-	.text
+.global _start, begtext, begdata, begbss, endtext, enddata, endbss
+.text
+begtext:
+.data
+begdata:
+.bss
+begbss:
+.text
 
-	.equ SETUPLEN, 4		# nr of setup-sectors(256*4=1024)
-	.equ BOOTSEG, 0x07c0	# original address of boot-sector
-	.equ INITSEG, DEF_INITSEG		# we move boot here - out of the way
-	.equ SETUPSEG, DEF_SETUPSEG		# setup starts here
-	.equ SYSSEG, DEF_SYSSEG			# system loaded at 0x10000 (65536).
-	.equ ENDSEG, SYSSEG + SYSSIZE	# where to stop loading
+.equ 	SETUPLEN, 4			# nr of setup-sectors(256*4=1024)
+.equ 	BOOTSEG,  0x07c0		# original address of boot-sector
+.equ 	INITSEG,  DEF_INITSEG		# we move boot here - out of the way
+.equ 	SETUPSEG, DEF_SETUPSEG		# setup starts here
+.equ 	SYSSEG,   DEF_SYSSEG		# system loaded at 0x10000 (65536).
+.equ 	ENDSEG,   SYSSEG + SYSSIZE	# where to stop loading
 
 # ROOT_DEV:	0x000 - same type of floppy as boot.
 #		0x301 - first partition on first drive etc
 #
 ##和源码不同，源码中是0x306 第2块硬盘的第一个分区
 #
-	.equ ROOT_DEV, 0x301
-    .equ SWAP_DEV, 0x301
-	ljmp $BOOTSEG, $_start #0x07c0:0005
+.equ 	ROOT_DEV, 0x301
+.equ 	SWAP_DEV, 0x301
+
+	ljmp	$BOOTSEG, $_start	#0x07c0:0005
 _start:
-	mov	$BOOTSEG, %ax	#将ds段寄存器设置为0x07C0
+	mov	$BOOTSEG, %ax		#将ds段寄存器设置为0x07C0
 	mov	%ax, %ds
-	mov	$INITSEG, %ax	#将es段寄存器设置为0x9000
+	mov	$INITSEG, %ax		#将es段寄存器设置为0x9000
 	mov	%ax, %es
 	mov	$256, %cx		#设置移动计数值256字
 	sub	%si, %si		#源地址	ds:si = 0x07C0:0x0000
 	sub	%di, %di		#目标地址 es:si = 0x9000:0x0000
-	rep					#重复执行并递减cx的值
+	rep				#重复执行并递减cx的值
 	movsw				#从内存[si]处移动cx个字到[di]处
-	ljmp $INITSEG, $go	#段间跳转，这里INITSEG指出跳转到的段地址，解释了cs的值为0x9000
+	ljmp	$INITSEG, $go		#段间跳转，这里INITSEG指出跳转到的段地址，解释了cs的值为0x9000
 go:	mov	%cs, %ax		#将ds，es，ss都设置成移动后代码所在的段处(0x9000)
 	mov	%ax, %ds
 	mov	%ax, %es
 # put stack at 0x9ff00.
 	mov	%ax, %ss
-	mov	$0xFF00, %sp	# arbitrary value >>512
+	mov	$0xFF00, %sp		# arbitrary value >>512
 
 # load the setup-sectors directly after the bootblock.
 # Note that 'es'(0x9000) is already set up. 
@@ -78,15 +79,15 @@ go:	mov	%cs, %ax		#将ds，es，ss都设置成移动后代码所在的段处(0x9
 ##es:bx ->指向数据缓冲区；如果出错则CF标志置位,ah中是出错码
 #
 load_setup:
-	mov	$0x0000, %dx	# drive 0, head 0
-	mov	$0x0002, %cx	# sector 2, track 0
-	mov	$0x0200, %bx	# address = 512, in INITSEG
-	.equ AX, 0x0200+SETUPLEN
-	mov $AX, %ax    	# service 2, nr of sectors
+	mov	$0x0000, %dx		# drive 0, head 0
+	mov	$0x0002, %cx		# sector 2, track 0
+	mov	$0x0200, %bx		# address = 512, in INITSEG
+	.equ	AX, 0x0200+SETUPLEN
+	mov	$AX, %ax		# service 2, nr of sectors
 	int	$0x13			# read it
-	jnc	ok_load_setup	# ok - continue
+	jnc	ok_load_setup		# ok - continue
 	mov	$0x0000, %dx
-	mov	$0x0000, %ax	# reset the diskette
+	mov	$0x0000, %ax		# reset the diskette
 	int	$0x13
 	jmp	load_setup
 
@@ -94,28 +95,31 @@ ok_load_setup:
 
 # Get disk drive parameters, specifically nr of sectors/track
 
-	mov	$0x00, %dl          # floppy
+	mov	$0x00, %dl		# floppy
 	mov	$0x0800, %ax		# AH=8 is get drive parameters
 	int	$0x13
-	#mov $0x00, %ch
-    and $0x003f, %cx
+	#mov 	$0x00, %ch
+	#seg 	cs
+    	and 	$0x003f, %cx
 	mov	%cx, %cs:sectors+0	# %cs means sectors is in %cs
-	mov	$INITSEG, %ax       # 0x9000 
+	mov	$INITSEG, %ax		# 0x9000 
 	mov	%ax, %es
 
 # Change: 2020-02-06
 # Clear screen
-    mov $0x0003, %ax
-    int $0x10
+    	mov $0x0003, %ax
+    	int $0x10
 
 # Print some inane message
 
-	mov	$0x03, %ah	    	# read cursor pos
+	mov	$0x03, %ah		# read cursor pos
 	xor	%bh, %bh
-	int	$0x10               # dh(row 0-24) dl(col 0-79)
-	mov	$32, %cx            # nr of characters
+	int	$0x10			# dh(row 0-24) dl(col 0-79)
+
+	mov	$32, %cx            	# nr of characters
 	mov	$0x000b, %bx		# page 0, attribute b 
-	mov $msg1, %bp
+	#lea	msg1, %bp
+	mov	$msg1, %bp
 	mov	$0x1301, %ax		# write string, move cursor
 	int	$0x10
 
@@ -124,17 +128,19 @@ ok_load_setup:
 
 	mov	$SYSSEG, %ax
 	mov	%ax, %es		# segment of 0x010000
-	call read_it
-	call kill_motor
+	call	read_it
+	call	kill_motor
 
 # After that we check which root-device to use. If the device is
 # defined (#= 0), nothing is done and the given device is used.
 # Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
 # on the number of sectors that the BIOS reports currently.
 
-	mov	%cs:root_dev+0, %ax #0x301
+	#seg 	cs
+	mov	%cs:root_dev+0, %ax 	# 0x301
 	cmp	$0, %ax
-	jne	root_defined        # ROOT_DEV has defined
+	jne	root_defined        	# ROOT_DEV has defined
+	#seg 	cs
 	mov	%cs:sectors+0, %bx
 	mov	$0x0208, %ax		# /dev/ps0 - 1.2Mb
 	cmp	$15, %bx
@@ -143,8 +149,9 @@ ok_load_setup:
 	cmp	$18, %bx
 	je	root_defined
 undef_root:
-	jmp undef_root
+	jmp 	undef_root
 root_defined:
+	#seg 	cs
 	mov	%ax, %cs:root_dev+0
 
 # after that (everyting loaded), we jump to
@@ -159,42 +166,44 @@ root_defined:
 #
 # in:	es - starting address segment (normally 0x1000)
 #
-sread:	.word 1+ SETUPLEN	# sectors read of current track
-head:	.word 0			    # current head
-track:	.word 0		    	# current track
+sread:	.word	1+ SETUPLEN		# sectors read of current track
+head:	.word	0			# current head
+track:	.word	0			# current track
 
 read_it:
 	mov	%es, %ax
-	test $0x0fff, %ax
+	test	$0x0fff, %ax
 die:
-    jne die			        # es must be at 64kB boundary
-	xor %bx, %bx		    # bx is starting address within segment
+	jne	die			# es must be at 64kB boundary
+	xor 	%bx, %bx		# bx is starting address within segment
 rp_read:
-	mov %es, %ax
- 	cmp $ENDSEG, %ax	    # have we loaded all yet?
+	mov 	%es, %ax
+	cmp	$ENDSEG, %ax		# have we loaded all yet?
 	jb	ok1_read
 	ret
 ok1_read:
+	#seg 	cs
 	mov	%cs:sectors+0, %ax
 	sub	sread, %ax
 	mov	%ax, %cx
 	shl	$9, %cx
 	add	%bx, %cx
-	jnc ok2_read
+	jnc 	ok2_read
 	je 	ok2_read
-	xor %ax, %ax
-	sub %bx, %ax
-	shr $9, %ax
+	xor 	%ax, %ax
+	sub 	%bx, %ax
+	shr 	$9, %ax
 ok2_read:
-	call read_track
-	mov %ax, %cx
-	add sread, %ax
-	cmp %cs:sectors+0, %ax
-	jne ok3_read
-	mov $1, %ax
-	sub head, %ax
-	jne ok4_read
-	incw track 
+	call 	read_track
+	mov 	%ax, %cx
+	add 	sread, %ax
+	#seg 	cs
+	cmp 	%cs:sectors+0, %ax
+	jne 	ok3_read
+	mov 	$1, %ax
+	sub 	head, %ax
+	jne 	ok4_read
+	incw    track 
 ok4_read:
 	mov	%ax, head
 	xor	%ax, %ax
@@ -210,10 +219,10 @@ ok3_read:
 	jmp	rp_read
 
 read_track:
-	push %ax
-	push %bx
-	push %cx
-	push %dx
+	push	%ax
+	push	%bx
+	push	%cx
+	push	%dx
 	mov	track, %dx
 	mov	sread, %cx
 	inc	%cx
@@ -245,7 +254,7 @@ bad_rt:	mov	$0, %ax
 # * don't have to worry about it later.
 # */
 kill_motor:
-	push %dx
+	push	%dx
 	mov	$0x3f2, %dx
 	mov	$0, %al
 	outsb
@@ -253,24 +262,24 @@ kill_motor:
 	ret
 
 sectors:
-	.word 0
+	.word	0
 
 msg1:
-	.byte 13,10
-	.ascii "linux 0.12 OS is booting ..."
-	.byte 13,10
+	.byte 	13,10
+	.ascii	"linux 0.12 OS is booting ..."
+	.byte	13,10
 
-	.org 506
+	.org	506
 swap_dev:
-    .word SWAP_DEV
+	.word 	SWAP_DEV
 root_dev:
-	.word ROOT_DEV
+	.word 	ROOT_DEV
 boot_flag:
-	.word 0xAA55
+	.word 	0xAA55
 	
-	.text
-	endtext:
-	.data
-	enddata:
-	.bss
-	endbss:
+.text
+endtext:
+.data
+enddata:
+.bss
+endbss:
--- code/boot/head.s
+++ code/boot/head.s
@@ -30,33 +30,34 @@ pg_dir:
 # 4. 通过ret跳转到init/main.c中的main运行
 .globl startup_32
 startup_32:
-    movl $0x10, %eax
-    mov %ax, %ds
-    mov %ax, %es
-    mov %ax, %fs
-    mov %ax, %gs
-    lss stack_start, %esp   # 设置系统堆栈
-    call setup_idt          # 设置中断描述符表
-    call setup_gdt          # 设置全局描述符表
-    
-    # 因为修改了gdt（段描述符中的段限长8MB改成了16MB），所以需要重新装载所有的段寄存器。CS代码段寄存器
-    # 已经在setup_gdt中重新加载过了。
+	movl 	$0x10,%eax
+	mov 	%ax,%ds
+	mov 	%ax,%es
+	mov 	%ax,%fs
+	mov 	%ax,%gs
+	lss 	stack_start,%esp	# 设置系统堆栈
+	call 	setup_idt		# 设置中断描述符表
+	call 	setup_gdt		# 设置全局描述符表
+	
+	# 因为修改了gdt（段描述符中的段限长8MB改成了16MB），所以需要重新装载所有的段寄存器。CS代码段寄存器
+	# 已经在setup_gdt中重新加载过了。
 
-    movl $0x10, %eax        # reload all the segment registers
-    mov %ax, %ds            # after changing gdt. CS was already
-    mov %ax, %es            # reloaded in 'setup_gdt'
-    mov %ax, %fs
-    mov %ax, %gs
-    lss stack_start, %esp
+	movl	$0x10,%eax		# reload all the segment registers
+	mov	%ax,%ds			# after changing gdt. CS was already
+	mov	%ax,%es			# reloaded in 'setup_gdt'
+	mov	%ax,%fs
+	mov	%ax,%gs
+	lss	stack_start,%esp
 
-    # 下面代码用于测试A20地址线是否已经开启
-    # 采用的方法是向内存地址0x000000处写入任意一个数值，然后看内存地址0x100000(1M)处是否也是这个数值。
-    # 如果一直相同的话（表示地址A20线没有选通），就一直比较下去，即死循环。
-    xorl %eax, %eax
-1:	incl %eax               # check that A20 really IS enabled
-    movl %eax, 0x000000     # loop forever if it isn't
-    cmpl %eax, 0x100000
-    je 1b
+	# 下面代码用于测试A20地址线是否已经开启
+	# 采用的方法是向内存地址0x000000处写入任意一个数值，然后看内存地址0x100000(1M)处是否也是这个数值。
+	# 如果一直相同的话（表示地址A20线没有选通），就一直比较下去，即死循环。
+	xorl	%eax,%eax
+1:	incl	%eax			# check that A20 really IS enabled
+	movl	%eax,0x000000		# loop forever if it isn't
+	cmpl	%eax,0x100000
+	je	1b
+
 /*
  * NOTE! 486 should set bit 16, to check for write-protect in supervisor
  * mode. Then it would be unnecessary with the "verify_area()"-calls.
@@ -74,13 +75,13 @@ startup_32:
  # 下面这段程序用于检查数学协处理器芯片是否存在
  # 方法是修改控制寄存器CR0，在假设存在协处理器的情况下执行一个协处理器指令，如果出错的话则说明协处理器
  # 芯片不存在，需要设置CR0中的协处理器仿真位EM(位2)，并复位协处理器存在标志MP(位1)。
-    movl %cr0, %eax						# check math chip
-    andl $0x80000011, %eax				# Save PG,PE,ET
-    /* "orl $0x10020,%eax" here for 486 might be good */
-    orl $2, %eax						# set MP
-    movl %eax, %cr0
-    call check_x87
-    jmp after_page_tables
+	movl	%cr0,%eax		# check math chip
+	andl	$0x80000011,%eax	# Save PG,PE,ET
+	#orl	$0x10020,%eax		# here for 486 might be good
+	orl	$2,%eax			# set MP
+	movl	%eax,%cr0
+	call	check_x87
+	jmp	after_page_tables
 
 /*
  * We depend on ET to be correct. This checks for 287/387.
@@ -96,20 +97,22 @@ startup_32:
 # 如果系统中存在协处理器的话，那么在执行了fninit指令后其状态字低字节肯定为0。
 
 check_x87:
-    fninit
-    fstsw %ax
-    cmpb $0, %al						# 初始化状态字应该为0,否则说明协处理器不存在
-    je 1f								/* no coprocessor: have to set bits */
-    movl %cr0, %eax
-    xorl $6, %eax						/* reset MP, set EM */
-    movl %eax, %cr0
-    ret
+	fninit
+	fstsw	%ax
+	cmpb	$0,%al			# 初始化状态字应该为0,否则说明协处理器不存在
+	je	1f			# no coprocessor: have to set bits
+	movl	%cr0,%eax
+	xorl	$6,%eax			# reset MP, set EM
+	movl	%eax,%cr0
+	ret
 
-.align 4 # 按4字节方式对齐内存地址， 为了提高32位CPU访问内存中代码或数据的速度和效率
-    # 两个字节值是80287协处理器指令fsetpm的机器码。其作用是把80287设置为保护模式。
-    # 80387无需该指令，并且将会把该指令看作是空操作
-1:	.byte 0xDB,0xE4		/* fsetpm for 287, ignored by 387 */	# 287协处理器码
-    ret
+# 按4字节方式对齐内存地址， 为了提高32位CPU访问内存中代码或数据的速度和效率
+# 两个字节值是80287协处理器指令fsetpm的机器码。其作用是把80287设置为保护模式。
+# 80387无需该指令，并且将会把该指令看作是空操作
+
+.align 4    
+1:	.byte 	0xDB,0xE4		# fsetpm for 287, ignored by 387  287协处理器码
+	ret
 
 /*
  *  setup_idt
@@ -130,22 +133,22 @@ check_x87:
  */
 
 setup_idt:
-    lea ignore_int, %edx
-    movl $0x00080000, %eax		# 将选择符0x0008置入eax的高16位中
-    movw %dx, %ax				/* selector = 0x0008 = cs */
-    movw $0x8E00, %dx			/* interrupt gate - dpl=0, present */	
-                                # 此时edx含有门描述符高4字节的值
+	lea 	ignore_int,%edx
+	movl 	$0x00080000,%eax	# 将选择符0x0008置入eax的高16位中
+	movw 	%dx,%ax			# selector = 0x0008 = cs
+	movw 	$0x8E00,%dx		# interrupt gate - dpl=0, present
+					# 此时edx含有门描述符高4字节的值
 
-    lea idt, %edi				# idt是中断描述符表的地址
-    mov $256, %ecx
+	lea 	idt,%edi		# idt是中断描述符表的地址
+	mov 	$256,%ecx
 rp_sidt:
-    movl %eax, (%edi)			# 将哑中断门描述符存入表中
-    movl %edx, 4(%edi)
-    addl $8, %edi				# edi指向表中下一项
-    dec %ecx
-    jne rp_sidt
-    lidt idt_descr				# 加载中断描述符表寄存器值
-    ret
+	movl	%eax,(%edi)		# 将哑中断门描述符存入表中
+	movl	%edx,4(%edi)
+	addl	$8,%edi			# edi指向表中下一项
+	dec	%ecx
+	jne	rp_sidt
+	lidt	idt_descr		# 加载中断描述符表寄存器值
+	ret
 
 /*
  *  setup_gdt
@@ -160,8 +163,8 @@ rp_sidt:
 
 # 加载全局描述符表寄存器(全局描述符表内容已设置好)
 setup_gdt:
-    lgdt gdt_descr
-    ret
+	lgdt 	gdt_descr
+	ret
 
 /*
  * I put the kernel page tables right after the page directory,
@@ -174,7 +177,8 @@ setup_gdt:
  */
 
  # 每个页表长为4KB字节(1页内存页面)
-.org 0x1000		# 从偏移0x1000处开始存放第1个页表
+ # 从偏移0x1000处开始存放第1个页表
+.org 0x1000
 pg0:
 
 .org 0x2000
@@ -198,7 +202,7 @@ pg3:
  */
 
 tmp_floppy_area:
-    .fill 1024,1,0
+	.fill	1024,1,0
 
 /***** 为跳转到init/main.c中的main()函数作准备工作 *****/
 # 前面3个入栈0值应该分别表示envp，argv指针和argc的值（main()没有用到）
@@ -206,47 +210,48 @@ tmp_floppy_area:
 # pushl $main  压入main函数的入口地址
 # 当head.s最后执行ret指令时就会弹出main()的地址
 after_page_tables:
-    pushl $0						# These are the parameters to main :-)
-    pushl $0						# 这些是调用main程序的参数(指init/main.c).
-    pushl $0
-    pushl $L6						# return address for main, if it decides to.
-    pushl $main
-    jmp setup_paging				# 跳转至setup_paging
+	pushl	$0			# These are the parameters to main :-)
+	pushl	$0			# 这些是调用main程序的参数(指init/main.c).
+	pushl	$0
+	pushl	$L6			# return address for main, if it decides to.
+	pushl	$main
+	jmp	setup_paging		# 跳转至setup_paging
 L6:
-    jmp L6							# main should never return here, but
-                                    # just in case, we know what happens.
-                                    # main程序绝对不应该返回到这里，不过为了以防万一，所以
-                                    # 添加了该语句。这样我们就知道发生什么问题了。
+	jmp 	L6			# main should never return here, but
+					# just in case, we know what happens.
+                                    	# main程序绝对不应该返回到这里，不过为了以防万一，所以
+                                    	# 添加了该语句。这样我们就知道发生什么问题了。
 
 /* This is the default interrupt "handler" :-) */
 /* 下面是默认的中断"向量句柄" */
 int_msg:
-    .asciz "Unknown interrupt\n\r"
+	.asciz	"Unknown interrupt\n\r"
 
 .align 4
 ignore_int:
-    pushl %eax
-    pushl %ecx
-    pushl %edx
-    push %ds
-    push %es
-    push %fs
-    
-    movl $0x10, %eax				# 设置段选择符(使ds，es，fs指向gdt表中的数据段)
-    mov %ax, %ds
-    mov %ax, %es
-    mov %ax, %fs
-    pushl $int_msg
-    call printk						# 该函数在kernel/printk.c中
-    popl %eax
-    
-    pop %fs
-    pop %es
-    pop %ds
-    popl %edx
-    popl %ecx
-    popl %eax
-    iret							# 中断返回
+	pushl %eax
+	pushl %ecx
+	pushl %edx
+	push %ds
+	push %es
+	push %fs
+
+	movl $0x10,%eax			# 设置段选择符(使ds，es，fs指向gdt表中的数据段)
+	mov %ax,%ds
+	mov %ax,%es
+	mov %ax,%fs
+	pushl $int_msg
+	call printk			# 该函数在kernel/printk.c中
+	popl %eax
+
+	pop %fs
+	pop %es
+	pop %ds
+	popl %edx
+	popl %ecx
+	popl %eax
+	iret				# 中断返回
+
 
 /*
  * Setup_paging
@@ -289,37 +294,38 @@ ignore_int:
 # 初始化页目录表前4项和4个页表
 .align 4
 setup_paging:
-    movl $1024 * 5, %ecx				/* 5 pages - pg_dir+4 page tables */
-    xorl %eax, %eax
-    xorl %edi, %edi						/* pg_dir is at 0x000 */	
-                                        # 页目录从0x0000地址开始
-    cld;rep;stosl						# eax内容存到es:edi所指内存位置处,且edi增4.
+	movl	$1024*5,%ecx		# 5 pages - pg_dir+4 page tables
+	xorl	%eax,%eax
+	xorl	%edi,%edi		# pg_dir is at 0x000
+					# 页目录从0x0000地址开始
+	cld;rep;stosl			# eax内容存到es:edi所指内存位置处,且edi增4.
 
-     # 设置页目录表中的前4个页目录项
-     # 例如第1个页目录项：
-     #      页表所在地址 = 0x00001007 & 0xfffff000 = 0x1000
-     #      页表属性标志 = 0x00001007 & 0x00000fff = 0x07    表示该页存在,用户可读写.
-    movl $pg0 + 7, pg_dir				/* set present bit/user r/w */
-    movl $pg1 + 7, pg_dir + 4			/*  --------- " " --------- */
-    movl $pg2 + 7, pg_dir + 8			/*  --------- " " --------- */
-    movl $pg3 + 7, pg_dir + 12			/*  --------- " " --------- */
+	# 设置页目录表中的前4个页目录项
+	# 例如第1个页目录项：
+	#      页表所在地址 = 0x00001007 & 0xfffff000 = 0x1000
+	#      页表属性标志 = 0x00001007 & 0x00000fff = 0x07    表示该页存在,用户可读写.
+	movl	$pg0+7,pg_dir		/* set present bit/user r/w */
+	movl	$pg1+7,pg_dir+4		/*  --------- " " --------- */
+	movl	$pg2+7,pg_dir+8		/*  --------- " " --------- */
+	movl	$pg3+7,pg_dir+12	/*  --------- " " --------- */
 
-    # 设置4个页表中所有项的内容（共4096项），从最后一个页表的最后一项开始按倒退顺序填写
-    movl $pg3 + 4092, %edi			# edi->最后一页的最后一项.
-    movl $0xfff007, %eax			/* 16Mb - 4096 + 7 (r/w user,p) */
-    std								# 方向位置位，edi值递减(4字节)
-1:	stosl							/* fill pages backwards - more efficient :-) */
-    subl $0x1000, %eax				# 每填好一项，物理地址值减0x1000。
-    jge 1b							# 如果小于0则说明全填写好了
-    cld
-    # 设置页目录表基地址寄存器cr3（保存页目录表的物理地址）
-    xorl %eax, %eax					/* pg_dir is at 0x0000 */
-    movl %eax, %cr3					/* cr3 - page directory start */
-    # 设置启动使用分页处理(cr0的PG标志，位31)
-    movl %cr0, %eax
-    orl $0x80000000, %eax			# 添上PG标志
-    movl %eax, %cr0					/* set paging (PG) bit */
-    ret								/* this also flushes prefetch-queue */
+	# 设置4个页表中所有项的内容（共4096项），从最后一个页表的最后一项开始按倒退顺序填写
+	movl	$pg3+4092,%edi		# edi->最后一页的最后一项.
+    
+	movl	$0xfff007,%eax		/*  16Mb - 4096 + 7 (r/w user,p) */
+	std				# 方向位置位，edi值递减(4字节)
+1:	stosl				/* fill pages backwards - more efficient :-) */
+	subl	$0x1000,%eax		# 每填好一项，物理地址值减0x1000。
+	jge	1b			# 如果小于0则说明全填写好了
+	cld
+	# 设置页目录表基地址寄存器cr3（保存页目录表的物理地址）
+	xorl 	%eax,%eax		/* pg_dir is at 0x0000 */
+	movl 	%eax,%cr3		/* cr3 - page directory start */
+	# 设置启动使用分页处理(cr0的PG标志，位31)
+	movl	%cr0,%eax
+	orl	$0x80000000,%eax	# 添上PG标志
+	movl	%eax,%cr0		/* set paging (PG) bit */
+	ret				/* this also flushes prefetch-queue */
 
 # 在改变分页处理标志后要求使用转移指令刷新预取指令队列，这里用的是返回指令ret。
 # 该返回指令ret的另一个作用并跳转到/init/main.c程序去运行。
@@ -329,27 +335,28 @@ setup_paging:
 # 前2字节是描述符表的限长，后4字节是描述符表在线性地址空间中的32位基地址。
 .word 0
 idt_descr:
-    .word 256 * 8 - 1				# idt contains 256 entries
-    .long idt
+	.word	256*8-1			# idt contains 256 entries
+	.long	idt
 
-.align 4    # 这个对齐貌似多余
+# 这个对齐貌似多余
+.align 4    
 
 .word 0
 gdt_descr:
-    .word 256 * 8 - 1				# so does gdt (not that that's any
-    .long gdt						# magic number, but it works for me :^)
+	.word	256*8-1			# so does gdt (not that that's any
+	.long	gdt			# magic number, but it works for me :^)
 
 .align 8
 # 中断描述符表（空表）
-idt:	.fill 256, 8, 0					# idt is uninitialized
+idt:	.fill 	256,8,0			# idt is uninitialized
 
 # 全局描述符表
 # 前4项分别是空项(不用)、代码段描述符、数据段描述符、系统调用段描述符（没有使用）
 # 同时还预留了252项的空间，用于放置所创建任务的局部描述符(LDT)和对应的任务状态段TSS的描述符
 # (0-nul, 1-cs, 2-ds, 3-syscall, 4-TSS0, 5-LDT0, 6-TSS1, 7-LDT1, 8-TSS2 etc...)
 gdt:
-    .quad 0x0000000000000000			/* NULL descriptor */
-    .quad 0x00c09a0000000fff			/* 16Mb */		# 0x08，内核代码段，长度16MB
-    .quad 0x00c0920000000fff			/* 16Mb */		# 0x10，内核数据段，长度16MB
-    .quad 0x0000000000000000			/* TEMPORARY - don't use */
-    .fill 252, 8, 0						/* space for LDT's and TSS's etc */
+	.quad	0x0000000000000000	/* NULL descriptor */
+	.quad	0x00c09a0000000fff	/* 16Mb */	# 0x08，内核代码段，长度16MB
+	.quad	0x00c0920000000fff	/* 16Mb */	# 0x10，内核数据段，长度16MB
+	.quad	0x0000000000000000	/* TEMPORARY - don't use */
+	.fill	252,8,0			/* space for LDT's and TSS's etc */
