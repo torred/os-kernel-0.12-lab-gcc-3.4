5d987b91ed6623c6e239c99d8f7a27dfe2e1580c
Todd <e-royal@163.com>
2022-10-15 23:58:08 +0800

    修改bug

--- code/boot/setup.s
+++ code/boot/setup.s
@@ -1,4 +1,4 @@
-	.code16
+.code16
 # rewrite with AT&T syntax by falcon <wuzhangjin@gmail.com> at 081012
 #
 #	setup.s		(C) 1991 Linus Torvalds
@@ -17,84 +17,85 @@
 # NOTE! These had better be the same as in bootsect.s!
 #include <linux/config.h>
 
-	.equ INITSEG, DEF_INITSEG	# we move boot here - out of the way
-	.equ SYSSEG, DEF_SYSSEG	# system loaded at 0x10000 (65536).
-	.equ SETUPSEG, DEF_SETUPSEG	# this is the current segment
+.equ	INITSEG, DEF_INITSEG		# we move boot here - out of the way
+.equ	SYSSEG, DEF_SYSSEG		# system loaded at 0x10000 (65536).
+.equ	SETUPSEG, DEF_SETUPSEG		# this is the current segment
 
-	.global _start, begtext, begdata, begbss, endtext, enddata, endbss
-	.text
-	begtext:
-	.data
-	begdata:
-	.bss
-	begbss:
-	.text
+.global _start, begtext, begdata, begbss, endtext, enddata, endbss
+.text
+begtext:
+.data
+begdata:
+.bss
+begbss:
+.text
 
-	ljmp $SETUPSEG, $_start	#phaddr: 0x90205
+	ljmp	$SETUPSEG, $_start		#phaddr: 0x90205
 _start:
-	mov %cs,%ax         #0x9020
-	mov %ax,%ds
-	mov %ax,%es
+	mov	%cs,%ax				#0x9020
+	mov	%ax,%ds
+	mov	%ax,%es
 #
 ##print some message
 #
-	mov $0x03, %ah
-	xor %bh, %bh
-	int $0x10
-	mov $27, %cx
-	mov $0x000b,%bx
-	mov $msg2,%bp
-	mov $0x1301, %ax
-	int $0x10
+	mov	$0x03, %ah
+	xor	%bh, %bh
+	int	$0x10
+
+	mov	$27, %cx
+	mov	$0x000b,%bx
+	mov	$msg2,%bp
+	mov	$0x1301, %ax
+	int	$0x10
 
 # ok, the read went well so we get current cursor position and save it for
 # posterity.
-	mov	$INITSEG, %ax	# this is done in bootsect already, but...
+	mov	$INITSEG, %ax		# this is done in bootsect already, but...
 	mov	%ax, %ds
-	mov	$0x03, %ah	    # read cursor pos
+	mov	$0x03, %ah	    	# read cursor pos
 	xor	%bh, %bh
-	int	$0x10		    # save it in known place, con_init fetches
-	mov	%dx, %ds:0	    # it from 0x90000.
+	int	$0x10		    	# save it in known place, con_init fetches
+	mov	%dx, %ds:0	    	# it from 0x90000.
 
 # Get memory size (extended mem, kB)
-    mov $INITSEG, %ax
-    mov %ax, %ds 
+    	mov	$INITSEG, %ax
+    	mov	%ax, %ds
 	mov	$0x88, %ah 
 	int	$0x15
-	mov	%ax, %ds:2      # save the value of extended memory size in 0x90002,
-                        # which excludes 1MB. (it's 15MB in this case)
+	mov	%ax, %ds:2		# save the value of extended memory size in 0x90002,
+					# which excludes 1MB. (it's 15MB in this case)
 
 # Get video-card data:
-    mov $INITSEG, %ax
-    mov %ax, %ds 
+    	mov	$INITSEG, %ax
+    	mov	%ax, %ds 
 	mov	$0x0f, %ah
 	int	$0x10
-	mov	%bx, %ds:4	    # bh = display page (0x00)
-	mov	%ax, %ds:6	    # al = video mode, ah = window width (0x50,0x03)
-                        # 80x25(col*row)
+	mov	%bx, %ds:4		# bh = display page (0x00)
+	mov	%ax, %ds:6		# al = video mode, ah = window width (0x50,0x03)
+					# 80x25(col*row)
 
 # check for EGA/VGA and some config parameters
-    mov $INITSEG, %ax
-    mov %ax, %ds
+	mov	$INITSEG, %ax
+	mov	%ax, %ds
 	mov	$0x12, %ah
 	mov	$0x10, %bl
 	int	$0x10
-	mov	%ax, %ds:8      # ???
-	mov	%bx, %ds:10     # bh = video mode bl = video memory size(256KB)
-	mov	%cx, %ds:12     # EGA 80x25
+	mov	%ax, %ds:8		# ???
+	mov	%bx, %ds:10		# bh = video mode bl = video memory size(256KB)
+	mov	%cx, %ds:12		# EGA 80x25
 
-    mov $0x5019, %ax    # 80x25
-    mov %ax, %ds:14
+	mov	$0x5019, %ax		# 80x25
+	mov	%ax, %ds:14
 
 # Get hd0 data(fixed disk parameter table)
 
 	mov	$0x0000, %ax
 	mov	%ax, %ds
-	lds	%ds:4*0x41, %si # src
+	lds	%ds:4*0x41, %si		# src
 	mov	$INITSEG, %ax
 	mov	%ax, %es
-	mov	$0x0080, %di    # dst: 0x90080
-	mov	$0x10, %cx      # the size of FDPT is 16)
+	mov	$0x0080, %di		# dst: 0x90080
+	mov	$0x10, %cx		# the size of FDPT is 16)
 	rep
 	movsb
 
@@ -102,7 +103,7 @@ _start:
 
 	mov	$0x0000, %ax
 	mov	%ax, %ds
-	lds	%ds:4*0x46, %si #
+	lds	%ds:4*0x46, %si
 	mov	$INITSEG, %ax
 	mov	%ax, %es
 	mov	$0x0090, %di
@@ -111,79 +112,80 @@ _start:
 	movsb
 
 ## modify ds
-	mov $INITSEG,%ax
-	mov %ax,%ds
-	mov $SETUPSEG,%ax
-	mov %ax,%es
+	mov	$INITSEG,%ax
+	mov	%ax,%ds
+	mov	$SETUPSEG,%ax
+	mov	%ax,%es
 
 ##show cursor pos:
-	mov $0x03, %ah 
-	xor %bh,%bh
-	int $0x10
-	mov $11,%cx
-	mov $0x000a,%bx
-	mov $cur,%bp
-	mov $0x1301,%ax
-	int $0x10
+	mov	$0x03, %ah 
+	xor	%bh,%bh
+	int	$0x10
+	mov	$11,%cx
+	mov	$0x000a,%bx
+	mov	$cur,%bp
+	mov	$0x1301,%ax
+	int	$0x10
 ##show detail(row*col)
-	mov %ds:0 ,%ax
-	call print_hex
-	call print_nl
+	mov	%ds:0 ,%ax
+	call	print_hex
+	call	print_nl
 
 ##show memory size
-	mov $0x03, %ah
-	xor %bh, %bh
-	int $0x10
-	mov $12, %cx
-	mov $0x000a, %bx
-	mov $mem, %bp
-	mov $0x1301, %ax
-	int $0x10
+	mov	$0x03, %ah
+	xor	%bh, %bh
+	int	$0x10
+	mov	$12, %cx
+	mov	$0x000a, %bx
+	mov	$mem, %bp
+	mov	$0x1301, %ax
+	int	$0x10
+
 ##show detail(KB)
-	mov %ds:2 , %ax
-	call print_hex
+	mov	%ds:2 , %ax
+	call	print_hex
 
 ##show nr of cylinders
-	mov $0x03, %ah
-	xor %bh, %bh
-	int $0x10
-	mov $25, %cx
-	mov $0x000a, %bx
-	mov $cyl, %bp
-	mov $0x1301, %ax
-	int $0x10
+	mov	$0x03, %ah
+	xor	%bh, %bh
+	int	$0x10
+	mov	$25, %cx
+	mov	$0x000a, %bx
+	mov	$cyl, %bp
+	mov	$0x1301, %ax
+	int	$0x10
 ##show detail
-	mov %ds:0x80, %ax
-	call print_hex
-	call print_nl
+	mov	%ds:0x80, %ax
+	call	print_hex
+	call	print_nl
 
 ##show nr of heads
-	mov $0x03, %ah
-	xor %bh, %bh
-	int $0x10
-	mov $9, %cx
-	mov $0x000a, %bx
-	mov $head, %bp
-	mov $0x1301, %ax
-	int $0x10
+	mov	$0x03, %ah
+	xor	%bh, %bh
+	int	$0x10
+	mov	$9, %cx
+	mov	$0x000a, %bx
+	mov	$head, %bp
+	mov	$0x1301, %ax
+	int	$0x10
 ##show detail
-	mov %ds:0x82, %ax
-	call print_hex
-	call print_nl
+	mov	%ds:0x82, %ax
+	call	print_hex
+	call	print_nl
 
 ##show nr of sectors per track 
-	mov $0x03, %ah
-	xor %bh, %bh
-	int $0x10
-	mov $9, %cx
-	mov $0x000a, %bx
-	mov $sect, %bp
-	mov $0x1301, %ax
-	int $0x10
+	mov	$0x03, %ah
+	xor	%bh, %bh
+	int	$0x10
+	mov	$9, %cx
+	mov	$0x000a, %bx
+	mov	$sect, %bp
+	mov	$0x1301, %ax
+	int	$0x10
 ##show detail
-	mov %ds:0x8e, %ax
-	call print_hex
-	call print_nl
+	mov	%ds:0x8e, %ax
+	call	print_hex
+	call	print_nl
 
 # Check that there IS a hd1 :-)
 
@@ -199,27 +201,27 @@ no_disk1:
 	mov	$0x0090, %di
 	mov	$0x10, %cx
 	mov	$0x00, %ax
-	rep             # clear       
+	rep             		# clear       
 	stosb
 is_disk1:
 
 # now we want to move to protected mode ...
 
-	cli			    # no interrupts allowed ! 
+	cli			    	# no interrupts allowed ! 
 
 # first we move the system to its rightful place(load kernel module)
 
 	mov	$0x0000, %ax
-	cld			    # 'direction'=0, movs moves forward
+	cld			    	# 'direction'=0, movs moves forward
 do_move:
-	mov	%ax, %es	# destination segment
+	mov	%ax, %es		# destination segment
 	add	$0x1000, %ax
-	cmp	$0x9000, %ax # 0x9000 SYS_SIZE 
+	cmp	$0x9000, %ax		# 0x9000 SYS_SIZE 
 	jz	end_move
-	mov	%ax, %ds	# source segment
+	mov	%ax, %ds		# source segment
 	sub	%di, %di
 	sub	%si, %si
-	mov $0x8000, %cx
+	mov	$0x8000, %cx
 	rep
 	movsw
 	jmp	do_move
@@ -227,13 +229,20 @@ do_move:
 # then we load the segment descriptors
 
 end_move:
-	mov	$SETUPSEG, %ax	# right, forgot this at first. didn't work :-)
-	mov	%ax, %ds        # 0x9020
-	lidt idt_48		    # load idt with 0,0
-	lgdt gdt_48		    # load gdt with whatever appropriate
+	mov	$SETUPSEG, %ax		# right, forgot this at first. didn't work :-)
+	mov	%ax, %ds		# 0x9020
+	lidt	idt_48			# load idt with 0,0
+	lgdt	gdt_48			# load gdt with whatever appropriate
 
 # that was painless, now we enable A20
 
+	#call	empty_8042	# 8042 is the keyboard controller
+	#mov	$0xD1, %al	# command write
+	#out	%al, $0x64
+	#call	empty_8042
+	#mov	$0xDF, %al	# A20 on
+	#out	%al, $0x60
+	#call	empty_8042
 	inb     $0x92, %al	# open A20 line(Fast Gate A20).
 	orb     $0b00000010, %al
 	outb    %al, $0x92
@@ -247,9 +256,9 @@ end_move:
 # have to reprogram the 8259's, and it isn't fun.
 
 	mov	$0x11, %al		# initialization sequence(ICW1)
-					    # ICW4 needed(1),CASCADE mode,Level-triggered
+					# ICW4 needed(1),CASCADE mode,Level-triggered
 	out	%al, $0x20		# send it to 8259A-1
-	.word   0x00eb,0x00eb		# jmp $+2, jmp $+2
+	.word	0x00eb,0x00eb		# jmp $+2, jmp $+2
 	out	%al, $0xA0		# and to 8259A-2
 	.word	0x00eb,0x00eb
 	mov	$0x20, %al		# start of hardware int's (0x20)(ICW2)
@@ -259,7 +268,7 @@ end_move:
 	out	%al, $0xA1		# from 0x28-0x2F
 	.word	0x00eb,0x00eb		# IR 7654 3210
 	mov	$0x04, %al		# 8259-1 is master(0000 0100)
-	out	%al, $0x21		# 
+	out	%al, $0x21		#
 	.word	0x00eb,0x00eb		# INT
 	mov	$0x02, %al		# 8259-2 is slave(010 --> 2)
 	out	%al, $0xA1
@@ -283,78 +292,83 @@ end_move:
 # things as simple as possible, we do no register set-up or anything,
 # we let the gnu-compiled 32-bit programs do that. We just jump to
 # absolute address 0x00000, in 32-bit protected mode.
-	mov	%cr0, %eax	# get machine status(cr0|MSW)	
-	bts	$0, %eax	# turn on the PE-bit 
-	mov	%eax, %cr0	# protection enabled
+	#mov	$0x0001, %ax		# protected mode (PE) bit
+	#lmsw	%ax			# This is it!
+	mov	%cr0, %eax		# get machine status(cr0|MSW)	
+	bts	$0, %eax		# turn on the PE-bit 
+	mov	%eax, %cr0		# protection enabled
 				
-				            # segment-descriptor        (INDEX:TI:RPL)
-	.equ	sel_cs0, 0x0008 # select for code segment 0 (  001:0 :00) 
-	ljmp	$sel_cs0, $0	# jmp offset 0 of code segment 0 in gdt
+					# segment-descriptor        (INDEX:TI:RPL)
+	.equ	sel_cs0, 0x0008 	# select for code segment 0 (  001:0 :00) 
+	ljmp	$sel_cs0, $0		# jmp offset 0 of code segment 0 in gdt
 
 gdt:
-	.word	0,0,0,0		# dummy
+	.word	0,0,0,0			# dummy
 
-	.word	0x07FF		# 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		# base address=0
-	.word	0x9A00		# code read/exec
-	.word	0x00C0		# granularity=4096, 386
+	.word	0x07FF			# 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x0000			# base address=0
+	.word	0x9A00			# code read/exec
+	.word	0x00C0			# granularity=4096, 386
 
-	.word	0x07FF		# 8Mb - limit=2047 (2048*4096=8Mb)
-	.word	0x0000		# base address=0
-	.word	0x9200		# data read/write
-	.word	0x00C0		# granularity=4096, 386
+	.word	0x07FF			# 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x0000			# base address=0
+	.word	0x9200			# data read/write
+	.word	0x00C0			# granularity=4096, 386
 
 idt_48:
 	.word	0			# idt limit=0
 	.word	0,0			# idt base=0L
 
 gdt_48:
-	.word	0x800			    # gdt limit=2048, 256 GDT entries
+	.word	0x800			# gdt limit=2048, 256 GDT entries
 	.word   512+gdt, 0x9		# gdt base = 0X9xxxx, 
 	# 512+gdt is the real gdt after setup is moved to 0x9020 * 0x10
 
 # print_hex()
 print_hex:
-	mov $4,%cx
-	mov %ax,%dx
+	mov	$4,%cx
+	mov	%ax,%dx
+
 print_digit:
-	rol $4,%dx	    #循环以使低4位用上，高4位移至低4位
-	mov $0xe0f,%ax  #ah ＝ 请求的功能值，al = 半个字节的掩码
-	and %dl,%al
-	add $0x30,%al
-	cmp $0x3a,%al
-	jl outp
-	add $0x07,%al
+	rol	$4,%dx	    		#循环以使低4位用上，高4位移至低4位
+	mov	$0xe0f,%ax  		#ah ＝ 请求的功能值，al = 半个字节的掩码
+	and	%dl,%al
+	add	$0x30,%al
+	cmp	$0x3a,%al
+	jl	outp
+	add	$0x07,%al
+
 outp:
-	int $0x10
-	loop print_digit
+	int	$0x10
+	loop	print_digit
 	ret
+
 #打印回车换行
 print_nl:
-	mov $0xe0d,%ax
-	int $0x10
-	mov $0xa,%al
-	int $0x10
+	mov	$0xe0d,%ax
+	int	$0x10
+	mov	$0xa,%al
+	int	$0x10
 	ret
 
 msg2:
-	.byte 13,10
-	.ascii "Now we are in setup ..."
-	.byte 13,10
+	.byte	13,10
+	.ascii	"Now we are in setup ..."
+	.byte	13,10
 cur:
-	.ascii "Cursor POS:"
+	.ascii	"Cursor POS:"
 mem:
-	.ascii "Memory SIZE:"
+	.ascii	"Memory SIZE:"
 cyl:
-	.ascii "KB"
-	.byte 13,10
-	.ascii "HD Info:"
-	.byte 13,10
-	.ascii " Cylinders:"
+	.ascii	"KB"
+	.byte	13,10
+	.ascii	"HD Info:"
+	.byte	13,10
+	.ascii	" Cylinders:"
 head:
-	.ascii " Headers:"
+	.ascii	" Headers:"
 sect:
-	.ascii " Secotrs:"
+	.ascii	" Secotrs:"
 .text
 endtext:
 .data
